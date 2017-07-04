8a0124a3d4f2e493ba7bdb9daef102f5cf4f6bb8
adrianiftode <adrian.iftode@gmail.com>
2017-07-04 10:11:14 +0300

    Use instance members to avoid compiler optimizations

--- src/Benchmark/StringsOperations.cs
+++ src/Benchmark/StringsOperations.cs
@@ -7,28 +7,35 @@ namespace Benchmark
     [MemoryDiagnoser, MarkdownExporter]
     public class StringsOperations
     {
+        string first = "A".PadLeft(50, 's');
+        string second = "B".PadLeft(50, 's');
+        public StringsOperations()
+        {
+            first = "A".PadLeft(50, 's');
+            second = "B".PadLeft(50, 's');
+        }
         [Benchmark]
         public void Concatenation()
         {
-	        var result = "first" + "second";
+	        var result = first + second;
         }
 
         [Benchmark]
         public void Format()
         {
-	        var result = string.Format("{0}{1}", "first", "second");
+	        var result = string.Format("{0}{1}", first, second);
 
         }
 	    [Benchmark]
 	    public void Interpolation()
 	    {
-		    var result = $"{"first"}{"second"}";
+		    var result = $"{first}{second}";
 	    }
 
 	    [Benchmark]
 	    public void StringBuilder()
 	    {
-		    var result = new StringBuilder().Append("first").Append("second").ToString();
+		    var result = new StringBuilder().Append(first).Append(second).ToString();
 	    }
 	}
 }
